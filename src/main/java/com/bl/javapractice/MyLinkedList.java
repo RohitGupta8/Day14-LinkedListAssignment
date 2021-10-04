8d5cc590aa807c6d35d14eb57066d8aed8a94845
RohitGupta8 <therohitg@gmail.com>
2021-10-04 15:07:23 +0530

    ADD : Ability to create Linked

---
+++ src/main/java/com/bl/javapractice/MyLinkedList.java
@@ -0,0 +1,35 @@
+package com.bl.javapractice;
+
+public class MyLinkedList {
+    public INode head;
+    public INode tail;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode myNode) {
+        if (this.tail == null) {
+            this.tail = myNode;
+        }
+        if (this.head == null) {
+            this.head = myNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = myNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    public void printMyNodes(){
+        StringBuffer myNodes =new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null){
+            myNodes.append(tempNode.getKey());
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+}
